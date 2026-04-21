ca817d1cef62d7bc30f440447a4302278e9e408a
fdygis-code <avishaysarin@gmail.com>
2026-04-21 16:48:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+
 importScripts('/scramjet/scramjet.bundle.js');
 
-const { ScramjetServiceWorker } = scramjet;
-const sw = new ScramjetServiceWorker();
+let sw;
+
+if (typeof ScramjetServiceWorker !== 'undefined') {
+    sw = new ScramjetServiceWorker();
+} else if (typeof __scramjet$bundle !== 'undefined') {
+    sw = new __scramjet$bundle.ScramjetServiceWorker();
+} else {
+    const key = Object.keys(self).find(k => k.includes('scramjet'));
+    if (key) sw = new self[key].ScramjetServiceWorker();
+}
 
 self.addEventListener('fetch', event => {
-    if (sw.route(event)) {
+    if (sw && sw.route(event)) {
         event.respondWith(sw.fetch(event));
     }
+});
